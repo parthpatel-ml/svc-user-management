b5d27c1db65757495229198396bdba04fe14b70c
parthpatel-ml <parthpatel.ml@gmail.com>
2024-08-30 00:37:32 +0930

    first

---
+++ src/main/java/com/insta/svc_user_management/SvcUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.insta.svc_user_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcUserManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insta/svc_user_management/SvcUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insta.svc_user_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
